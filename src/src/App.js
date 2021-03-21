df6cdcd1e20ada1098b98c08ceb2acd2f52b7e5a
rohitsingh11 <rohit@twodotseven.com>
2021-03-21 19:45:38 +0530

    added some fanctionality

--- src/src/App.js
+++ src/src/App.js
@@ -1,57 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import Posts from "./components/posts";
+import Favorite from "./components/favorite";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="heading">
+          <span>
+            <Link to="/">Posts</Link>
+          </span>
+          |
+          <span>
+            <Link to="/favpost">Favorite Post</Link>
+          </span>
+        </div>
+        <Switch>
+          <Route
+            exact
+            activeClassName="active"
+            path="/favpost"
+            component={Favorite}
+          ></Route>
+          <Route
+            exact
+            activeClassName="active"
+            path="/"
+            component={Posts}
+          ></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
